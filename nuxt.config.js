81dfd40ad10d7c8ee258f38f4f7e6006c2d7875d
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-14 13:00:19 +0900

    Fix タイトル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'todos_akisaka',
+    title: 'TODOリスト',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
