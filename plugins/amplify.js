2344ec7bdcde725ed90a59bce476abd050e75605
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-15 15:51:04 +0900

    Fix スペース調整

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,6 +1,6 @@
-import API from '@aws-amplify/api';
-import PubSub from '@aws-amplify/pubsub';
-import awsconfig from '../src/aws-exports';
- 
-API.configure(awsconfig);
-PubSub.configure(awsconfig);
+import API from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub'
+import awsconfig from '../src/aws-exports'
+
+API.configure(awsconfig)
+PubSub.configure(awsconfig)
