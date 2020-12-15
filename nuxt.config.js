3c96b5c0e9e3415f46aea867d15033fc7de49619
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-15 20:52:02 +0900

    fix lintfix

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '~/plugins/amplify.js'
-  ],
+  plugins: ['~/plugins/amplify.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,6 +1,4 @@
 import API from '@aws-amplify/api'
-import PubSub from '@aws-amplify/pubsub'
 import awsconfig from '../src/aws-exports'
 
 API.configure(awsconfig)
-PubSub.configure(awsconfig)
