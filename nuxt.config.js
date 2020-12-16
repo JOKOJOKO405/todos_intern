83ab20ff0a0c1c28dbf91a81320b2e3ae759b20c
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-16 12:34:21 +0900

    Add ssr:falseに

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
+  ssr: false,
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
