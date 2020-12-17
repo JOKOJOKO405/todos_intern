1a5af097e625eaeca62e926a758372677c1f0377
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-17 16:09:58 +0900

    Add wrapperコンポーネント追加

---
+++ components/BoardComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from '@nuxtjs/composition-api'
+import { TodoKey } from '~/composition/compositionKey'
+import { TodoStore } from '~/composition/useTodo'
+
+export default defineComponent({
+  setup() {
+    const { created } = inject(TodoKey) as TodoStore
+    return { created }
+  },
+})
+</script>
