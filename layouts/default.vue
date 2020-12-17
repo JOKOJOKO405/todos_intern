df7bf205c940e1a855a4724a075ca0eb6c86df7d
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-17 16:09:41 +0900

    Remove いらないクラス削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,31 +4,20 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, provide } from '@nuxtjs/composition-api'
-import { TodoKey } from '~/composition/compositionKey'
-import useTodo from '~/composition/useTodo'
-
-export default defineComponent({
-  setup() {
-    provide(TodoKey, useTodo())
-    return {}
-  },
-})
-</script>
-
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+* {
+  &,
+  &::before,
+  &::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 }
 </style>
