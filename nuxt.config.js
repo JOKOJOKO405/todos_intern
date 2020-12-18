7213e1321115266f23336cac180556a93bf0e20a
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-18 14:53:50 +0900

    Remove monent一旦削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,11 +25,7 @@ export default {
     '@nuxtjs/eslint-module',
     '@nuxt/typescript-build',
     '@nuxtjs/composition-api',
-    '@nuxtjs/monent',
   ],
-  moment: {
-    locales: ['ja'],
-  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [],
